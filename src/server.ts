30d7b7f6a4ae5e5e303e2c1951180a74e0213460
arthur <arthur.rios007@gmail.com>
2025-07-08 19:11:30 -0300

    chore: add initial config

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: 'http://localhost:5173',
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app
+  .listen({ port: process.env.PORT ? Number(process.env.PORT) : 3333 })
+  .then(() => {
+    console.log(`HTTP server running on port ${process.env.PORT}`)
+  })
